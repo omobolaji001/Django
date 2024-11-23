670c7aaeba87d5c6c7d09d8cfcca547c11509443
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-11-23 18:56:09 +0100

    Modified blog/views.py

--- tech_chronicles/blog/views.py
+++ tech_chronicles/blog/views.py
@@ -8,8 +8,10 @@ def post_list(request):
 
     return render(request, 'blog/post/list.html', {'posts': posts})
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     """ Returns a specific post based on post id """
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+    post = get_object_or_404(Post, status=Post.Status.PUBLISHED,
+                             slug=post, publish__year=year,
+                             publish__month=month, publish__day=day)
 
     return render(request, 'blog/post/detail.html', {'post': post})
