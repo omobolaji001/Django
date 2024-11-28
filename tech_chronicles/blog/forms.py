147c1772f93842984ad949962bbe6514673f4c2e
omobolaji001 <omobolajiolayiwola11@gmail.com>
2024-11-28 13:58:41 +0100

    Designed the Comment form

--- tech_chronicles/blog/forms.py
+++ tech_chronicles/blog/forms.py
@@ -1,10 +1,19 @@
 """ Defines Form classes """
 from django import forms
+from .models import Comment
 
 
-class EmailFormPost(forms.Form):
+class EmailPostForm(forms.Form):
     """ Defines form for sharing posts through email """
     name = forms.CharField(max_length=25)
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False, widget=forms.Textarea)
+
+
+class CommentForm(forms.ModelForm):
+    """ Defines a form for comments on blog posts """
+    class Meta:
+        """ metadata """
+        model = Comment
+        fields = ['name', 'email', 'body']
